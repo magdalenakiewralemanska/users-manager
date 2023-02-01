9996bd6ab93cba5baa9bf5b55197fcb00a231ab2
Magda K-L <magdalenazur86@gmail.com>
2023-02-01 11:01:42 +0100

    application operation test

---
+++ src/main/java/com/sdacodecoolproject/usersmanager/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.sdacodecoolproject.usersmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @GetMapping("/home")
+    public String showUser(){
+        return "everything is correct right now :)";
+    }
+}
