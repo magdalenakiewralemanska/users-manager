2d56bbc0e4551ced62e6918028eb4215050dbd95
Magda K-L <magdalenazur86@gmail.com>
2023-02-01 10:44:17 +0100

    CurrentUser class repair - Granted Authority added

--- src/main/java/com/sdacodecoolproject/usersmanager/model/CurrentUser.java
+++ src/main/java/com/sdacodecoolproject/usersmanager/model/CurrentUser.java
@@ -1,9 +1,12 @@
 package com.sdacodecoolproject.usersmanager.model;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class CurrentUser implements UserDetails {
 
@@ -15,7 +18,7 @@ public class CurrentUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return (Arrays.stream(this.user.getAuthorities()).map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
     }
 
     @Override
