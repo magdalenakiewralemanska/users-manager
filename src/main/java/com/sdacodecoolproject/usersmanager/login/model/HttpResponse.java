c6556d1ee21e4da6a1e49886c17be0859569ecf0
Magda K-L <magdalenazur86@gmail.com>
2023-02-01 21:50:52 +0100

    timestamp added in HttpResponse

--- src/main/java/com/sdacodecoolproject/usersmanager/login/model/HttpResponse.java
+++ src/main/java/com/sdacodecoolproject/usersmanager/login/model/HttpResponse.java
@@ -1,17 +1,28 @@
 package com.sdacodecoolproject.usersmanager.login.model;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class HttpResponse {
 
     private Integer statusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss", timezone = "Poland")
+    private Date timeStamp;
+
+    public HttpResponse(Integer statusCode, HttpStatus httpStatus, String reason, String message) {
+        this.statusCode = statusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+        this.timeStamp = new Date();
+    }
 }
