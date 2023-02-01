2c44d24a1416117ff5868836e6abae58a889db61
Magda K-L <magdalenazur86@gmail.com>
2023-02-01 14:06:39 +0100

    continuation of token config - getting claims for user and token, configure verifier

--- src/main/java/com/sdacodecoolproject/usersmanager/constant/SecurityConstant.java
+++ src/main/java/com/sdacodecoolproject/usersmanager/constant/SecurityConstant.java
@@ -6,5 +6,8 @@ public class SecurityConstant {
     public static final String GET_ARRAYS_ADMINISTRATION = "User Manager";
     public static final String AUTHORITIES = "Authorities";
     public static final long EXPIRATION_TIME = 259_200_000;
+    public static final String NOT_VERIFIED_TOKEN = "Token not verified";
+
+
 
 }
--- src/main/java/com/sdacodecoolproject/usersmanager/security/token/TokenProvider.java
+++ src/main/java/com/sdacodecoolproject/usersmanager/security/token/TokenProvider.java
@@ -1,16 +1,23 @@
 package com.sdacodecoolproject.usersmanager.security.token;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.sdacodecoolproject.usersmanager.constant.SecurityConstant;
 import com.sdacodecoolproject.usersmanager.model.CurrentUser;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TokenProvider {
-    @Value("jwt.secret")
-    private String secret;
+    @Value(value = "jwt.secret")
+    private static String secret;
 
     public String generateToken(CurrentUser currentUser){
         String[] claims = getClaimsFromUser(currentUser);
@@ -23,9 +30,37 @@ public class TokenProvider {
                 .sign(Algorithm.HMAC256(secret.getBytes()));
     }
 
-    private static String[] getClaimsFromUser(CurrentUser currentUser) {
+    public List<GrantedAuthority> getAuthorities(String token){
+        List<String> claims = getClaimsFromToken(token);
+        return claims.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
 
-        return null;
+    private static List<String> getClaimsFromToken(String token) {
+        JWTVerifier verifier = getVerifier();
+        return verifier.verify(token)
+                .getClaim(SecurityConstant.AUTHORITIES)
+                .asList(String.class);
+    }
+
+    private static JWTVerifier getVerifier() {
+        JWTVerifier verifier;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            verifier = JWT.require(algorithm).withIssuer(SecurityConstant.GET_ARRAYS_LLC).build();
+        } catch (JWTVerificationException e) {
+            throw new JWTVerificationException(SecurityConstant.NOT_VERIFIED_TOKEN);
+        }
+        return verifier;
+    }
+
+    private static String[] getClaimsFromUser(CurrentUser currentUser) {
+        List<String> authorities = new ArrayList<>();
+        for(GrantedAuthority grantedAuthority: currentUser.getAuthorities()){
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
     }
 
 }
